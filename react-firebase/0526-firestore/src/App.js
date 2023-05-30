dba0938cb609bf15a553377f7524e3567dac766e
daeuk heo <gjeodnr2@gmail.com>
2023-05-30 17:44:40 +0900

    230530

---
+++ react-firebase/0526-firestore/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import BookComp from './components/BookComp';
+
+function App() {
+  return (
+    <div className="App">
+      <BookComp />
+    </div>
+  );
+}
+
+export default App;
